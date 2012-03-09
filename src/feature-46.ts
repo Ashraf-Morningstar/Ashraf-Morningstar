9990abd73bce07e085a88894162611b54ce5ad79
Ashraf-Morningstar <Ashraf-Morningstar@users.noreply.github.com>
2012-03-09 08:10:00 +0000

    feat(feature-46): update feature logic

--- src/feature-46.ts
+++ src/feature-46.ts
@@ -1,20 +1,21 @@
-interface FeatureToggle {
-  id: number;
-  name: string;
-  isActive: boolean;
-}
+// src/feature-46.ts
+export class Feature46Processor {
+  private id: number;
+  private status: string;
 
-const getFeatureStatus = (feature: FeatureToggle): string => {
-  if (feature.isActive) {
-    return `Feature ${feature.id} (${feature.name}) is currently ON.`;
+  constructor(id: number) {
+    this.id = id;
+    this.status = 'initialized';
   }
-  return `Feature ${feature.id} (${feature.name}) is currently OFF.`;
-};
 
-const feature46: FeatureToggle = {
-  id: 46,
-  name: "Advanced Analytics Dashboard",
-  isActive: true,
-};
+  public processData(data: string): string {
+    this.status = 'processing';
+    console.log(`Feature 46: Processing data for ID ${this.id}: "${data}"`);
+    this.status = 'completed';
+    return `Processed data for ${this.id}: ${data.toUpperCase()}`;
+  }
 
-console.log(getFeatureStatus(feature46));
+  public getStatus(): string {
+    return `ID ${this.id} status: ${this.status}`;
+  }
+}
