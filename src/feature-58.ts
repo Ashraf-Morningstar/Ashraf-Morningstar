6ee8d7db797fd92a22e99898ae6f8df931fdeb4a
Ashraf-Morningstar <Ashraf-Morningstar@users.noreply.github.com>
2012-03-09 05:02:00 +0000

    feat(feature-58): Update logic in feature 58

--- src/feature-58.ts
+++ src/feature-58.ts
@@ -1,2 +1,12 @@
-// Content for src/feature-58.ts
-console.log('Hello World');
+interface FeatureToggle {
+  id: number;
+  name: string;
+  isEnabled: boolean;
+}
+
+export function initializeFeature(feature: FeatureToggle): string {
+  if (feature.isEnabled) {
+    return `Feature ${feature.name} (ID: ${feature.id}) is active.`;
+  }
+  return `Feature ${feature.name} (ID: ${feature.id}) is inactive.`;
+}
