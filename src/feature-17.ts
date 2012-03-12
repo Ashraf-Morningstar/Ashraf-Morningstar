9858afca359fff24eb3b4a4fb20820254cfefab2
Ashraf-Morningstar <Ashraf-Morningstar@users.noreply.github.com>
2012-03-12 04:04:00 +0000

    feat(feature-17): update logic

--- src/feature-17.ts
+++ src/feature-17.ts
@@ -1,2 +1,18 @@
-// Content for src/feature-17.ts
-console.log('Hello World');
+interface FeatureFlags {
+  id: number;
+  name: string;
+  isActive: boolean;
+}
+
+export const feature17Data: FeatureFlags = {
+  id: 17,
+  name: "Enhanced User Dashboard",
+  isActive: true,
+};
+
+export function toggleFeatureStatus(feature: FeatureFlags): FeatureFlags {
+  return { ...feature, isActive: !feature.isActive };
+}
+
+const updatedFeature = toggleFeatureStatus(feature17Data);
+console.log(`Feature ${updatedFeature.id} is now ${updatedFeature.isActive ? 'active' : 'inactive'}.`);
